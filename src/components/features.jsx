7431efc539077ca9cd8667ad801afa801219a08e
adjelritmo <renionapp@gmail.com>
2025-04-20 00:23:04 +0100

    first commit

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -11,14 +11,14 @@ const Features = () => {
                         <p>Edite o código em tempo real com a sua equipa.</p>
                     </div>
                     <div className="col-md-4 p-4 text-center btn btn-outline-info">
-                        <i className="fas fa-users fs-3 display-4"></i>
+                        <i className="fas fa-users display-4"></i>
                         <h3>Colaboração em Equipa</h3>
                         <p>Convide os membros da sua equipa para participarem na sua sessão.</p>
                     </div>
                     <div className="col-md-4 p-4 text-center btn btn-outline-info">
                         <i className="fas fa-save display-4"></i>
-                        <h3>Save Sessions</h3>
-                        <p>Save your sessions for later use.</p>
+                        <h3>Salvar sessões</h3>
+                        <p>Guarde as suas sessões para utilização posterior.</p>
                     </div>
                 </div>
             </div>
