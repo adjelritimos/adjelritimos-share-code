a00816b6219ae0f0fd4cd4d468e3f30df5ca475e
adjelritmo <renionapp@gmail.com>
2025-04-21 19:34:46 +0100

    atualizando

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
@@ -13,4 +13,4 @@ export default defineConfig({
   server: {
     historyApiFallback: true, // Redireciona todas as rotas para index.html
   },
-});
+})
