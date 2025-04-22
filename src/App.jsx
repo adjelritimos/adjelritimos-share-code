7fb49507cfa56c535efb867962294509382b91ba
adjelritmo <renionapp@gmail.com>
2025-04-22 13:24:26 +0100

    add files

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import SessionNoCount from './screens/sessionNoSession'
 
 const App = () => {
     return (
-      <Router>
+      <Router basename='/adjelritimos-share-code.onrender.com//'>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/session" element={<Session />} />
