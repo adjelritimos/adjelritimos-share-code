46d40f9fad3575ecb519776ea8fac7bb3dea0e12
adjelritmo <renionapp@gmail.com>
2025-04-25 10:21:33 +0100

    mmmmm

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,23 @@ import Session from './screens/session'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import SessionNoCount from './screens/sessionNoSession'
 
+
+const Routers = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/session" element={<Session />} />
+        <Route path="/nocountsession" element={<SessionNoCount />} />
+      </Routes>
+    </Router>
+  )
+}
+
 const App = () => {
-    return (
-      <Router basename='/adjelritimos-share-code.onrender.com/'>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/session" element={<Session />} />
-            <Route path="/nocountsession" element={<SessionNoCount />} />
-          </Routes>
-      </Router>
-    )
-  }
+  return (
+    <Routers />
+  )
+}
 
 export default App
